31d8e0f93f68528327e7785abc32549c06317d51
otto67 <otto_munthe@yahoo.no>
2021-10-07 10:20:49 +0200

    Updated code

--- include/FirstClass.h
+++ include/FirstClass.h
@@ -11,13 +11,14 @@ class FirstClass {
         int (* FCWorker)(int, int*);
 
     public:
-        FirstClass() {myInt = 0;}
-        FirstClass(int a) {myInt = a;};
-        ~FirstClass(){if (myIntPointer != nullptr) {delete myIntPointer; std::cout<<"Freeing memory \n"; } };
+        FirstClass() : myInt(0){}
+        FirstClass(int a) : myInt(a){}
+
+        ~FirstClass(){if (myIntPointer != nullptr) delete myIntPointer; };
         void setFCWorker(int (*a)(int, int*));
         virtual int getInt();
         int doStuffFirst();
-        void allocMem(){myIntPointer = new int[3]; myIntPointer[0] = 1; myIntPointer[1] = 2; myIntPointer[2] = 3;}                
+        void allocMem(int a=1, int b=2, int c=3){myIntPointer = new int[3]; myIntPointer[0] = a; myIntPointer[1] = b; myIntPointer[2] = c;}                
 };
 
 class FirstDerivedClass : public FirstClass, public SecondClass {
@@ -27,8 +28,8 @@ class FirstDerivedClass : public FirstClass, public SecondClass {
 
     public:
         FirstDerivedClass(){mySecondInt = 0;}
-        FirstDerivedClass(int a, int b) : FirstClass (a) {mySecondInt = b;}
-        FirstDerivedClass(int a, int b, float c) : FirstClass (a), SecondClass(c)  {mySecondInt = b;}
+        FirstDerivedClass(int a, int b) : FirstClass (a), mySecondInt(b){} 
+        FirstDerivedClass(int a, int b, float c) : FirstClass (a), SecondClass(c), mySecondInt(b){}
         int getInt() override;
 };
 
--- src/SecondClass.cpp
+++ src/SecondClass.cpp
@@ -4,14 +4,14 @@
       
 int SecondClass::doStuffSecond(){
     if (!SCWorker){
-        std::cout<<"No worker provided \n";
         return -1;
     }
 
     myData tmp {10, 20};
     
+    this->myFloat = 8;
+    
     return SCWorker((void*) & tmp).a;
-
 }
 
 void SecondClass::printData(){
