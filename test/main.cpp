69f0cb6a3b3134a9a03af35a4550ac5497ab670f
otto67 <otto_munthe@yahoo.no>
2021-10-07 10:26:38 +0200

    Updates

---
+++ test/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <SecondClass.h>
+#include <FirstClass.h>
+
+int myFunc(int a, int* b){
+    return a + (*b);
+}
+
+int main(int nargs, char** args){
+
+    int x1, x2;
+    float x3;
+    bool passed = true;
+
+    FirstClass a(2);
+    a.allocMem(1, 1, 1);
+    a.setFCWorker(myFunc);
+    x1 = a.getInt();
+
+    if (x1 == 9)
+        std::cout<<"Test 1: PASS \n";
+    else {
+        std::cout<<"Test 1: FAIL \n";
+        passed = false;
+    }
+
+    SecondClass b(2.7);
+    x3 = b.getNumber();
+
+    if (x3 == 2.7f)
+        std::cout<<"Test 2: PASS \n";
+    else {
+        std::cout<<"Test 2: FAIL \n";
+        passed = false;
+    }
+
+    FirstDerivedClass c(10, 20, 2.0);
+    x2 = c.getInt();
+
+    if (x2 == 30)
+        std::cout<<"Test 2: PASS \n";
+    else {
+        std::cout<<"Test 2: FAIL \n";
+        passed = false;
+    }
+    if (passed)
+        return 0;
+    else 
+        return -1;
+}
