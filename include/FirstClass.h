fa9444136a7d3291d0166f049292ee292ab9d2e7
otto67 <otto_munthe@yahoo.no>
2021-10-07 23:54:36 +0200

    Updates

--- include/FirstClass.h
+++ include/FirstClass.h
@@ -11,15 +11,6 @@ class FirstClass {
         int (* FCWorker)(int, int*);
 
     public:
-<<<<<<< HEAD
-        FirstClass() {myInt = 0;}
-        FirstClass(int a) {myInt = a;};
-        ~FirstClass(){if (myIntPointer != nullptr) { delete myIntPointer; std::cout<<"Freeing memory \n"; } };
-        void setFCWorker(int (*a)(int, int*));
-        virtual int getInt();
-        int doStuffFirst( );
-        void allocMem(){myIntPointer = new int[3]; myIntPointer[0] = 1; myIntPointer[1] = 2; myIntPointer[2] = 3;}                
-=======
         FirstClass() : myInt(0){}
         FirstClass(int a) : myInt(a){}
 
@@ -28,7 +19,6 @@ class FirstClass {
         virtual int getInt();
         int doStuffFirst();
         void allocMem(int a=1, int b=2, int c=3){myIntPointer = new int[3]; myIntPointer[0] = a; myIntPointer[1] = b; myIntPointer[2] = c;}                
->>>>>>> develop
 };
 
 class FirstDerivedClass : public FirstClass, public SecondClass {
